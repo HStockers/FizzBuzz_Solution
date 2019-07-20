b66a8090a17107ab825cbc594c523e65001aa3d5
HStockers <52888837+HStockers@users.noreply.github.com>
2019-07-20 18:08:42 +0100

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+// i mean its easy if you put your head to it
+for (var i = 1 <=100; i++;) {
+
+
+var output ="";
+if(i % 3 == 0) { output += "Fizz";}
+if(i % 5 == 0) { output += "Buzz";}
+
+if(output == "") {output = i; }
+
+console.log(output)
+
+}
